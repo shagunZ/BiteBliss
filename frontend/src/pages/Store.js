e867d6b10ec0714ac41c264cc16af365c8974192
Anirudh Salaria <salariaanirudh@gmail.com>
2023-11-19 16:41:10 +0530

    frontend

--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import {Row,Col} from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 // import {productsArray} from '../productsStore'
-import {productsArray} from '../BiteBliss'
+import { productsArray } from '../BiteBliss'
 import ProductCard from '../components/ProductCard'
 const Store = () => {
   return (
-    <div>
-        <h1 align="center" className='p-3' style={{color:"brown"}}>Welcome to our store!</h1>
+    <div className=''>
+      <div className='storebanner'>
+        <div className='storebannerdata'>
+          <div className='store1'>FRESH AND DELICIOUS FOOD DAILY</div>
+          <div className='store2'>Lorem Ipsum, Dolor Sit Amet Consectetur Adipisicing Elit. Placeat Labore, Sint Cupiditate Distinctio Tempora Reiciendis.</div>
+          <div>
+            <button className='storebtn'>
+              EXPLORE
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ color: "white", fontSize: '35px', padding: '15px ', backgroundColor:'#010103' }}>Welcome to our store!</div>
+      <div style={{padding:'10px 80px', backgroundColor:'#010103', paddingBottom:'85px'}}>
         <Row xs={1} md={3} className='g-4'>
-
-            {productsArray.map((product,index)=>(
+          {productsArray.map((product, index) => (
             <Col align="center" key={index}>
-                <ProductCard product={product} />
-                </Col>
+              <ProductCard product={product} />
+            </Col>
 
-            ))}
-
+          ))}
         </Row>
+      </div>
     </div>
   )
 }
